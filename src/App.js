81365aebdad4015755b4ea17798070c116bac568
faressiouane <fares.geek.dz@gmail.com>
2020-12-14 22:06:50 +0100

    installed material ui + created ButtonAppBar component, added simple image grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+import Container from '@material-ui/core/Container';
+
+import ButtonAppBar from './components/ButtonAppBar';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  responsive_image: {
+    width: "100%",
+    height: "auto",
+  },
+}));
 
 function App() {
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <ButtonAppBar />
+     <Container maxWidth="lg">
+    <Grid container spacing={3}>
+        <Grid  item xs={12} sm={12} md={6} lg={4} xl={4}>
+          <Paper className={classes.paper}>
+              <img src="https://via.placeholder.com/600/56a8c2" className={classes.responsive_image}/>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12} sm={12} md={6} lg={4} xl={4}>
+          <Paper className={classes.paper}>
+              <img src="https://via.placeholder.com/600/56a8c2" className={classes.responsive_image}/>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12} sm={12} md={6} lg={4} xl={4}>
+          <Paper className={classes.paper}>
+              <img src="https://via.placeholder.com/600/56a8c2" className={classes.responsive_image}/>
+          </Paper>
+        </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
