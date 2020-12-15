3251a7f0c75415150aa4a976528c1ba66d7ffc47
faressiouane <fares.geek.dz@gmail.com>
2020-12-15 20:14:46 +0100

    added load more button and functionality

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 
 import ButtonAppBar from './components/ButtonAppBar';
+import Button from '@material-ui/core/Button'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,10 +28,12 @@ const useStyles = makeStyles((theme) => ({
 
 function App() {
  
-  //states
   const classes = useStyles();
-  const [images, setImages] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+
+  //states
+  const [images, setImages] = useState([]); // the final imgs list that follow all requirements!
+  const [visibleImages, setVisibleImages] = useState(10); // how many imgs to show
+  const [isLoading, setIsLoading] = useState(true); //loading state 
 
 // custom reducer for grouping images based on their albumId:
   const groupByAlbumId = (acc, val)=>{
@@ -53,15 +56,23 @@ function App() {
       const images = result.data.filter(img => img.albumId % 2 === 0); //reciving the result & filtring it to get imgs with even albumId only
       const albums = images.reduce(groupByAlbumId, {}) //grouping that belong to the same album (share the same albumId value) into an object using my custom reducer
       const imgs = [] // initialize imgs var to recive the 1st img from each album (used later to initialize images state)
+     
       for (const album in albums) { //looping let i around albums, taking the 1st img, assign it to imgs
         imgs.push(albums[album][0]);
       }
+      
+      //reset images_list to render it, and loading state
       setImages(imgs)
       setIsLoading(false);
     };
  
     fetchData();
   }, []);
+
+  // show more 10 imgs
+  const onloadMore = () => {
+      setVisibleImages(prev => prev + 10)
+  }
 
   return (
     <div className={classes.root}>
@@ -71,7 +82,7 @@ function App() {
     <Grid container spacing={3}>
 
           {isLoading && <h1>loading images ...!</h1>}
-          {images.map(img => (
+          {images.slice(0, visibleImages).map(img => (
                     <Grid  item xs={12} sm={12} md={6} lg={4} xl={4} key={img.id}>
                       <Paper className={classes.paper}>
                       albumId : {img.albumId} || imageId : {img.id} 
@@ -79,6 +90,17 @@ function App() {
                       </Paper>
                     </Grid>
           ))}
+
+        {/* render the button only if the visible img are less than 50, and isLoading = false */}
+        {visibleImages < 50 && !isLoading && (
+        <Grid item sm={12} md={12} lg={12} xl={12}>
+          <Button variant="contained" color="primary"
+          onClick={onloadMore}
+          >
+             Load more
+             </Button>
+        </Grid>
+        )}
 
         </Grid>
       </Container>
