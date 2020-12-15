c99162bb583f48b068d58d4412e88e7ee4881acc
faressiouane <fares.geek.dz@gmail.com>
2020-12-15 19:31:56 +0100

    fixed bug, was about miss assigning photos

--- src/App.js
+++ src/App.js
@@ -31,27 +31,8 @@ function App() {
   const classes = useStyles();
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  
-  const [error, setError] = useState(null)
 
-  //lifecycle hooks
-  // useEffect(()=>{
-  //   fetch('https://jsonplaceholder.typicode.com/photos')
-  //     .then(res => res.json())
-  //     .then(
-  //       (img) => {
-  //         const images = img.filter(img => img.albumId % 2 === 0);
-  //         for (let i = 0; i < images.length; i++) {
-  //           console.log(images[i][0]);
-  //         }
-  //         console.log(images);
-  //       },
-  //       (error) => {
-  //         setIsLoading(true)
-  //         setError(error)
-  //       }
-  //     )
-  // },[])
+// custom reducer for grouping images based on their albumId:
   const groupByAlbumId = (acc, val)=>{
     // Group initialization
     if (!acc[val.albumId]) {
@@ -62,17 +43,19 @@ function App() {
     return acc;
   }
 
+//component did mount:
   useEffect(() => {
+    //axios fetch api
     const fetchData = async () => {
       const result = await axios(
         'https://jsonplaceholder.typicode.com/photos',
       );
-      const images = result.data.filter(img => img.albumId % 2 === 0);
-      const groups = images.reduce(groupByAlbumId, {})
-      const imgs = []
-           for (let i = 2; i < 51; i += 2) {
-             imgs.push(groups[i][0]) 
-            }
+      const images = result.data.filter(img => img.albumId % 2 === 0); //reciving the result & filtring it to get imgs with even albumId only
+      const albums = images.reduce(groupByAlbumId, {}) //grouping that belong to the same album (share the same albumId value) into an object using my custom reducer
+      const imgs = [] // initialize imgs var to recive the 1st img from each album (used later to initialize images state)
+      for (const album in albums) { //looping let i around albums, taking the 1st img, assign it to imgs
+        imgs.push(albums[album][0]);
+      }
       setImages(imgs)
       setIsLoading(false);
     };
@@ -87,15 +70,14 @@ function App() {
 
     <Grid container spacing={3}>
 
-          {error && <h1>{error.message}</h1>}
           {isLoading && <h1>loading images ...!</h1>}
           {images.map(img => (
                     <Grid  item xs={12} sm={12} md={6} lg={4} xl={4} key={img.id}>
                       <Paper className={classes.paper}>
-                          {img.id} || {img.albumId}
+                      albumId : {img.albumId} || imageId : {img.id} 
                           <img src={img.url} className={classes.responsive_image} alt={img.title}/>
                       </Paper>
-                  </Grid>
+                    </Grid>
           ))}
 
         </Grid>
